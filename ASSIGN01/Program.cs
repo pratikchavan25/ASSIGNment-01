b0474629949d6ddb6f15947957481157b8373f2f
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-09 12:26:52 +0530

    Add project files.

---
+++ ASSIGN01/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//1.Accept radius of a circle. Display area of a circle.
+Console.WriteLine("Enter readius of a circle");
+
+double radius = Convert.ToDouble(Console.ReadLine());
+
+double Area  = Math.PI * radius * radius;
+
+Console.WriteLine("Area of Circle: " + Area);
+
+Console.ReadLine();
+
